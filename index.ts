d1d6466df2712eb1e9cce5139023fd54914301ea
Visal Dinuwara <visaldinuwara@gmail.com>
2026-05-06 03:01:33 +0530

    Add initial implementation of pizza ordering system

--- index.ts
+++ index.ts
@@ -1,22 +1,26 @@
 const { setEngine } = require("node:crypto")
 
-let menu = [
+type  pizzaType={name:string,price:number}
+type orderType={id:number,pizza:pizzaType,status:string}
+
+let menu:pizzaType[]= [
   { name: "Margerita", price: 10 },
   { name: "Pepperoni", price: 12 },
   { name: "Hawaiian", price: 11 }
 ]
-let cashInRegister = 100
-let orderQueue = []
-let orderId = 1
+let cashInRegister:number = 100
+let orderQueue:orderType[] = []
+let orderId:number = 1
 
 function addMenuItem({ name, price }) {
   menu.push({ name, price })
 }
 
-function placeOrder(itemName) {
-  const selectedPizza = menu.find((item) => item.name === itemName)
+function placeOrder(itemName:string) {
+  const selectedPizza= menu.find((item) => item.name === itemName)
+  if(selectedPizza){
   cashInRegister += selectedPizza.price
-  const newOrder = {
+  const newOrder:orderType = {
     id: orderId++,
     pizza: selectedPizza,
     status: "Ordered"
@@ -24,12 +28,15 @@ function placeOrder(itemName) {
   orderId++
   orderQueue.push(newOrder)
 }
+}
 
 function completeOrder(orderId) {
   const order = orderQueue.find((orderObj) => orderObj.id === orderId)
-  if (order) {
-    order.status = "Completed"
+  if (!order) {
+    console.log(`${orderId} order is not found!`)
+    return
   }
+  order.status = "Completed"
   return order
 }
 
